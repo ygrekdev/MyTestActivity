7e7777dcb0f1f403ee0f285e44d1719f56d2eeb8
ygrek <ygrekdev@gmailcom>
2024-03-01 19:01:47 +0200

    1 events

--- app/src/main/java/com/example/mytestactivity/AboutActivity.java
+++ app/src/main/java/com/example/mytestactivity/AboutActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,5 +22,41 @@ public class AboutActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("test", "About onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("test", "About onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("test", "About onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("test", "About onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("test", "About onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("test", "About onRestart()");
+        super.onRestart();
     }
 }
--- app/src/main/java/com/example/mytestactivity/MainActivity.java
+++ app/src/main/java/com/example/mytestactivity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("test", "Main onCreate()");
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
@@ -23,5 +25,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("test", "Main onStart()");
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("test", "Main onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("test", "Main onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("test", "Main onResume()");
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("test", "Main onRestart()");
+        super.onRestart();
     }
 }
