4a096d47c85143b0e2e037af16119a8591116288
ygrek <ygrekdev@gmailcom>
2024-03-01 18:50:01 +0200

    0

---
+++ app/src/main/java/com/example/mytestactivity/AboutActivity.java
@@ -0,0 +1,25 @@
+package com.example.mytestactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
